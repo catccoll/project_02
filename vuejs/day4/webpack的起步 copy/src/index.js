71105c2d4643dc1ce290af14ed00d8385982b7ee
gao <1078632735@qq.com>
2021-10-11 09:02:40 +0800

    11

--- vuejs/day4/webpack的起步 copy/src/index.js
+++
@@ -1,9 +0,0 @@
-// 1.使用commonjs的模块化规范
-const { add, sub } = require("../src/mathUtil.js")
-console.log(add(1, 2));
-console.log(sub(2, 1));
-// 1.使用ES6的模块化规范
-import { height, age, name } from "../src/info.js"
-console.log(name);
-console.log(height);
-console.log(age);
--- vuejs/day4/webpack的起步 copy/src/info.js
+++
@@ -1,3 +0,0 @@
-export const name = 'xsg'
-export const age = 18
-export const height = 1.99
